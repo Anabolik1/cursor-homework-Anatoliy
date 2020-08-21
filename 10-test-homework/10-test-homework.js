f242cb85c0f4c3b60a9446235696bfa8a940fc15
Anatoliy <kiriiienko2016@gmail.com>
2020-08-21 20:12:21 +0300

    hw11 (1version)

---
+++ 10-test-homework/10-test-homework.js
@@ -0,0 +1,37 @@
+
+
+const btn = document.getElementById("btn1");
+
+function getRandomChinese(length)
+{
+	return new Promise((resolve)=>{
+
+	setTimeout(()=>{
+
+		let i = 0;
+		let arr = [];
+		let sign = String(Date.now()).slice(-5);
+
+			while( i < length ){
+				sign+=1;
+				arr.push(String.fromCharCode(sign));
+				i++;
+				 
+        }
+
+	resolve( arr.join(""))},length*50);
+
+    });
+
+}
+
+
+ btn.addEventListener("click", ()=>{
+
+ 	getRandomChinese(5).then((result => {
+ 		
+ 		document.getElementById("result").innerHTML = result;
+
+ 	})
+ })
+	
