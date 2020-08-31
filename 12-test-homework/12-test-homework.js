09b1e749e8783e56722dc9ceb98112bf28f7526a
Anatoliy <kiriiienko2016@gmail.com>
2020-08-31 13:53:12 +0300

    hw13

---
+++ 12-test-homework/12-test-homework.js
@@ -0,0 +1,48 @@
+const res = document.getElementById(`result`);
+let fontSize = 14;
+let bool;
+
+
+function* generatot()
+{
+	let i = 0;
+  while (true) {
+    yield i++;
+  }
+}
+
+const Generator = generatot();
+
+
+document.getElementById(`generator`).addEventListener("click",()=> {
+res.innerHTML = Generator.next().value;
+});
+
+
+function* upSize () {
+
+while(bool ===true)
+	yield(fontSize+=2);
+}
+
+function* downSize () {
+
+while( bool ===false )
+	yield( fontSize-=2 );
+}
+
+
+let sizeUp = upSize();
+let sizeDown = downSize();
+
+document.getElementById(`up`).addEventListener("click",()=> {
+	bool = true;
+res.style.fontSize = `${sizeUp.next().value}px`;
+});
+
+
+document.getElementById(`down`).addEventListener("click",()=> {
+	bool = false;
+res.style.fontSize = `${sizeDown.next().value}px`;
+});
+
